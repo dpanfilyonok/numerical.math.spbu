8e110547175093f06ebff4d012056ecd91523528
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-09-06 12:04:55 +0300

    Add template for lab 1

---
+++ Lab01.NonlinearEquations/Library.fs
@@ -0,0 +1,10 @@
+namespace Lab01.NonlinearEquations
+
+module NonlinearEquations = 
+
+    let rootSeparation a b h = 
+        let n = (b - a) / h
+        for i in 1 .. n do
+            
+        
+
