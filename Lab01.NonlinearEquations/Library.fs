6746165d8b67b961c82222e7e4f14e88aa81f261
Dmitriy Panfilyonok <dmitriy.panfilyonok@gmail.com>
2019-09-16 22:00:04 +0300

    Add root separation

--- Lab01.NonlinearEquations/Library.fs
+++ Lab01.NonlinearEquations/Library.fs
@@ -1,10 +1,29 @@
 namespace Lab01.NonlinearEquations
 
 module NonlinearEquations = 
+    open System
 
-    let rootSeparation a b h = 
-        let n = (b - a) / h
-        for i in 1 .. n do
-            
+    type Methods = 
+        | Bisection
+        | SimpleIteration
+        | Newton
+        | Secant
+
+
+    let splitSection (a: float) (b: float) (h: float) = 
+        Seq.unfold (fun leftBorder -> 
+            if Math.Abs (leftBorder - b) < 1e-6 then None
+            else
+                let rightBoarder = min b (leftBorder + h)
+                Some ((leftBorder, rightBoarder), rightBoarder)) a
+
+    let separateRoots (f: float -> float) a b h = 
+        splitSection a b h
+        |> Seq.filter (fun x -> (f <| fst x) * (f <| snd x) <= 0.)
+
+    let solveEq (method: Methods) = 
+        match method with
+        | Bisection -> 
         
-
+    
+    let s = solveEq Methods.Newton
