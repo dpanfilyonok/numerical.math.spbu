78bdc3cb04eebcdeac0d6afdd6b7ae532602ba61
Dmitriy Panfilyonok <dmitriy.panfilyonok@gmail.com>
2019-12-02 17:59:06 +0300

    Add lab 6

--- Lab06.HighAccuracyQuadratureRules/HighAccuracyQuadratureRules.fs
+++ Lab06.HighAccuracyQuadratureRules/HighAccuracyQuadratureRules.fs
@@ -7,47 +7,23 @@ module HighAccuracyQuadratureRules =
     open NonlinearEquations
     open NumericalIntegration
     open System.Runtime.InteropServices
+    open MathNet.Numerics.Integration
 
-    /// w = 1
-    let compositeGaussLegendreQuadrature (f: float -> float) (segment: LineSegment) n m = 
-        let normalizedSegment = {Left = -1.; Right = 1.}
-        let legendrePoly = 
-            legendrePolynomials
-            |> Seq.take n
-            |> Seq.skip (n - 2)
-            |> Seq.toArray
-        let nodes = EquationSolveTask(legendrePoly.[1], normalizedSegment).Solve ()
-        let coefficients = 
-            nodes 
-            |> List.map (fun node ->
-                2. * (1. - node ** 2.) / 
-                (float n ** 2. * legendrePoly.[0] node ** 2.))
-        let gaussianRule (segment: LineSegment) =
-            [0 .. (n - 1)]
-            |> List.sumBy (fun k ->
-                coefficients.[k] * f (segment.Length / 2. * nodes.[k] + segment.Middle))
-            |> (*) <| segment.Length / 2. 
-        
-        segment
-        |> Segment.splitSegmentIntoEqualParts m
-        |> Seq.sumBy gaussianRule   
+    let twoNodesGaussianQuadrature (f: float -> float) (w: float -> float) (segment: LineSegment) 
+            ([<Out>] log: 
+            {|  moments: (float * float * float * float)
+                a1a2: (float * float)
+                nodes: (float * float) 
+                coeffs: (float * float) 
+            |} option byref) = 
 
-    let gaussLegendreQuadrature (f: float -> float) (segment: LineSegment) n =
-        compositeGaussLegendreQuadrature f segment n 1 
-
-    let chebyshevGaussQuadrature (f: float -> float) n = 
-        getChebyshevRoots n
-        |> List.sumBy f
-        |> (*) <| Math.PI / float n
-
-
-    let twoNodesGaussianQuadrature (f: float -> float) (w: float -> float) (segment: LineSegment) ([<Out>] info : bool byref) = 
         let n = 2
         let moments = 
             [0 .. (2 * n - 1)]
             |> List.map (fun k -> 
-                IntegrationTask((fun x -> w x * x ** float k), segment)
-                    .SolveWith SimpsonsRule 10)
+                // IntegrationTask((fun x -> w x * x ** float k), segment)
+                //     .SolveWith SimpsonsRule 10)
+                GaussLegendreRule.Integrate((fun x -> w x * x ** float k), segment.Left, segment.Right, 10)) // ???
             |> List.toArray
 
         let a1 = 
@@ -60,11 +36,57 @@ module HighAccuracyQuadratureRules =
         let (x1, x2) = 
             EquationSolveTask((fun x -> x ** 2. + a1 * x + a2), segment)
             |> Equation.solve
-            |> (fun lst -> lst.[0], lst.[1])
+            |> fun lst -> lst.[0], lst.[1]
 
-        let aBig1 = 
-            (moments.[1] - x2 * moments.[0]) / (x1 - x2)
-        let aBig2 = 
-            (moments.[1] - x1 * moments.[0]) / (x2 - x1)
+        let coeff1 = (moments.[1] - x2 * moments.[0]) / (x1 - x2)
+        let coeff2 = (moments.[1] - x1 * moments.[0]) / (x2 - x1)
         
-        aBig1 * f x1 + aBig2 * f x2
+        let methodLog =
+            {| 
+                moments = (moments.[0], moments.[1], moments.[2], moments.[3])
+                a1a2 = (a1, a2)
+                nodes = (x1, x2)
+                coeffs = (coeff1, coeff2)
+            |} |> Some
+
+        log <- methodLog
+        coeff1 * f x1 + coeff2 * f x2
+
+    /// w = 1
+    let compositeGaussLegendreQuadrature (f: float -> float) (segment: LineSegment) n m = 
+        let normalizedSegment = {Left = -1.; Right = 1.}
+
+        let (previousPolynomial, nthPolynomial) = 
+            legendrePolynomials
+            |> Seq.take (n + 1)
+            |> Seq.skip (n - 1)
+            |> Seq.toArray
+            |> fun arr -> (arr.[0], arr.[1])
+
+        let nodes = 
+            EquationSolveTask(nthPolynomial, normalizedSegment)
+            |> Equation.solve
+
+        let coefficients = 
+            nodes 
+            |> List.map (fun node ->
+                2. * (1. - node ** 2.) / 
+                (float n ** 2. * previousPolynomial node ** 2.))
+                
+        let gaussLegendreQuadrature (segment: LineSegment) =
+            [0 .. (n - 1)]
+            |> List.sumBy (fun k ->
+                coefficients.[k] * f (segment.Length / 2. * nodes.[k] + segment.Middle))
+            |> (*) <| segment.Length / 2. 
+        
+        segment
+        |> Segment.splitSegmentIntoEqualParts m
+        |> Seq.sumBy gaussLegendreQuadrature   
+
+    let gaussLegendreQuadrature (f: float -> float) (segment: LineSegment) n =
+        compositeGaussLegendreQuadrature f segment n 1 
+
+    let chebyshevGaussQuadrature (f: float -> float) n = 
+        getChebyshevRoots n
+        |> List.sumBy f
+        |> (*) <| Math.PI / float n
--- Lab06.HighAccuracyQuadratureRules/Program.fs
+++ Lab06.HighAccuracyQuadratureRules/Program.fs
@@ -1,8 +1,38 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿namespace NumericalMethods
 
-open System
+module Lab06 = 
+    open System
+    open HighAccuracyQuadratureRules
+    open Utils
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let f = Math.Sin
+    // let w = fun (x: float) -> 1.
+    let w = Math.Sqrt >> (/) 1.
+
+    let a = -1.
+    let b = 1.
+
+    let lab6input () = 
+        printfn "Введите пределы интегрирования a и b (a <= b): "
+        let segment = {Left = Console.ReadLine () |> float; Right = Console.ReadLine () |> float}
+        printfn "Введите число узлов для использования в квадратурных формулах (N > 0): "
+        let n = Console.ReadLine () |> int
+        printfn "Введите число разбиений для составной формулы Гаусса (m > 0): "
+        let m = Console.ReadLine () |> int
+
+        (segment, n, m)
+
+    [<EntryPoint>]
+    let main argv =
+        printfn "Лабораторная работа №6"
+        printfn "Приближённое вычисление интегралов при помощи квадратурных формул Наивысшей Алгебраической Степени Точности"
+
+        while true do
+            let (segment, n, m) = lab6input ()
+            printfn "1) Интеграл при помощи КФ Гаусса = %.8f" (compositeGaussLegendreQuadrature f segment n m)
+            let mutable log = None
+            printfn "2) Интеграл при помощи КФ типа Гаусса = %.8f" (twoNodesGaussianQuadrature f w segment &log)
+            printfn "%A" log
+            printfn "3) Интеграл при помощи КФ Мелера = %.8f" (chebyshevGaussQuadrature f n)
+
+        0
